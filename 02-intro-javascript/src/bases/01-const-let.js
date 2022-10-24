6c1d606c0e189571775c53fd57a57dc5aa7d47d8
Santiago Espinosa <santiago.espinosa@globant.com>
2022-10-24 15:01:10 -0500

    Create new subproject js basics

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// Variables and constants
+
+const name = 'Santiago';
+const lastname = 'Espinosa';
+
+let myValue = 5;
+myValue = 4;
+
+console.log(name, lastname, myValue);
+
+// var shouldn't be used anymore
+
+if (true) {
+  const name = 'Peter';
+  console.log(name);
+}
+
+console.log(myValue);
