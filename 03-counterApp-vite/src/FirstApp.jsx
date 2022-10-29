f739d895cc86c7994f89889716f78dd16decb85e
sesp13 <sespinosarte99@gmail.com>
2022-10-29 12:40:45 -0500

    Finish react basics projects, and apply useEffect hook

--- 03-counterApp-vite/src/FirstApp.jsx
+++ 03-counterApp-vite/src/FirstApp.jsx
@@ -1,8 +1,22 @@
-export const FirstApp = () => {
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subtitle, name }) => {
   return (
     <>
-      <h1>Santiago Espinosa</h1>
-      <p>Soy un subtitulo</p>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <p>{name}</p>
     </>
   );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.number.isRequired,
+};
+
+FirstApp.defaultProps = {
+  name: 'Fernando herrera',
+  subtitle: 'No hay subtítulo',
+  title: 'No hay título',
 };
--- 03-counterApp-vite/src/main.jsx
+++ 03-counterApp-vite/src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
+// Import css
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <HelloWorldApp /> */}
-    <FirstApp></FirstApp>
+    {/* <FirstApp></FirstApp> */}
+    <CounterApp value={0} />
   </React.StrictMode>
 );
