12092ece82a39d987f908496ff1b3ffab70dfa80
sesp13 <sespinosarte99@gmail.com>
2022-12-06 21:38:57 -0500

    05-hooksapp: Add mini project for useContext

---
+++ 05-hooks-app/src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { UserContext } from './UserContext';
+
+// const user = { id: 123, name: 'Fernando' };
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
