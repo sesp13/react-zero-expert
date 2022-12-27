386fd09777f0998763735bdd30072ab542d8d0dc
Santiago Espinosa <santiago.espinosa@globant.com>
2022-12-27 16:57:11 -0500

    07-heroesApp: Add characters list/card

--- 07-heroes-spa/src/heroes/index.js
+++ 07-heroes-spa/src/heroes/index.js
@@ -2,3 +2,4 @@ export * from './components';
 export * from './pages';
 export * from './hooks';
 export * from './routes';
+export * from './helpers';
--- 07-heroes-spa/src/heroes/pages/DcPage.jsx
+++ 07-heroes-spa/src/heroes/pages/DcPage.jsx
@@ -1,7 +1,11 @@
+import { HeroList } from '../components';
+
 export const DcPage = () => {
   return (
     <>
-      <h1>Dc Page</h1>
+      <h1>Dc Comics</h1>
+      <hr />
+      <HeroList publisher="DC Comics" />
     </>
   );
 };
