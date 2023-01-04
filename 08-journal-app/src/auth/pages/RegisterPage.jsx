ebba3f203a631e947a0763ded384b3a25bada0bd
sesp13 <sespinosarte99@gmail.com>
2023-01-04 16:17:58 -0500

    08-journal: add mui config and auth views

---
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,60 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
+export const RegisterPage = () => {
+  return (
+    <AuthLayout title='Register'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Name"
+              type="text"
+              placeholder="Your name"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@email.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Your password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Signup
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{mr: 1}}>Already registered?</Typography>
+            <Link color="inherit" to="/auth/login" component={RouterLink}>
+              Login
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
---
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import { MailOutline } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+
+export const JournalPage = () => {
+  return (
+    <>
+      <Typography>JournalPage</Typography>;
+      <MailOutline />
+    </>
+  );
+};
