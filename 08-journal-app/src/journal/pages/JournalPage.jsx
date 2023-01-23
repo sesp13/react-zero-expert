857caded0bbd1f7d54083b36fcf5076e15f78d45
sesp13 <sespinosarte99@gmail.com>
2023-01-22 21:38:57 -0500

    add first journal actions

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,29 +1,39 @@
 import { AddOutlined, MailOutline } from '@mui/icons-material';
 import { IconButton, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { startNewNote } from '../../store/journal';
 import { JournalLayout } from '../layout';
 import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const { isSaving, active: activeNote } = useSelector((state) => state.journal);
+
+  const onNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
-      <NothingSelectedView />
-      {/* <NoteView /> */}
+      {!!activeNote ? <NoteView /> : <NothingSelectedView />}
 
       <IconButton
+        onClick={onNewNote}
+        disabled={isSaving}
         size="large"
         sx={{
           color: 'white',
           backgroundColor: 'error.main',
           ':hover': {
             backgroundColor: 'error.main',
-            opacity: 0.9
+            opacity: 0.9,
           },
           position: 'fixed',
           right: 50,
-          bottom: 50
+          bottom: 50,
         }}
       >
-        <AddOutlined sx={{fontSize: 30}} />
+        <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
     </JournalLayout>
   );
---
+++ 08-journal-app/src/store/journal/journalSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+  name: 'journal',
+  initialState: {
+    isSaving: false,
+    messageSaved: '',
+    notes: [],
+    active: null,
+  },
+  reducers: {
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
+    addNewEmptyNote: (state, { payload }) => {
+      // payload is a new note
+      state.notes.push(payload);
+      state.isSaving = false;
+    },
+    setActiveNote: (state, { payload }) => {
+      // payload is a new note
+      state.active = payload;
+    },
+    setNotes: (state, action) => {},
+    setSaving: (state, action) => {},
+    updateNote: (state, action) => {},
+    deleteNoteById: (state, action) => {},
+  },
+});
+
+export const {
+  savingNewNote,
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById,
+} = journalSlice.actions;
