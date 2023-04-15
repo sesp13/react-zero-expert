7bd8270e44a6773ef67aa258d634a737ae1beb78
sesp13 <sespinosarte99@gmail.com>
2023-04-14 23:05:52 -0500

    10-calendar: add local testing configuration

---
+++ 10-calendar/jest.setup.js
@@ -0,0 +1,10 @@
+// En caso de tener variables de entorno y aún no soporta el import.meta.env
+// yarn add -D dotenv
+require('dotenv').config({
+    path: '.env.test'
+});
+
+// Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnvVariables', () => ({
+    getEnvVariables: () => ({ ...process.env })
+}));
